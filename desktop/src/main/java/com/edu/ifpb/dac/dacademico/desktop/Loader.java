633cf24c07f4b8acdac17c51557087463e24c94e
Douglas Gabriel <douglasxgabriel@gmail.com>
2015-07-15 15:43:49 -0300

    implementando método main que invoca rotina para extrair entidades da url e jogar no banco

---
+++ desktop/src/main/java/com/edu/ifpb/dac/dacademico/desktop/Loader.java
@@ -0,0 +1,17 @@
+package com.edu.ifpb.dac.dacademico.desktop;
+
+import com.edu.ifpb.dac.dacademico.core.servico.professor.ProfessorService;
+import com.edu.ifpb.dac.dacademico.core.servico.professor.ProfessorServiceImpl;
+import java.io.IOException;
+
+/**
+ *
+ * @author douglasgabriel
+ * @version 0.1
+ */
+public class Loader {
+    public static void main(String[] args) throws IOException {
+        ProfessorServiceImpl service = new ProfessorServiceImpl("com.edu.ifpb.dac.dacademico_entidades_jar_1.0-SNAPSHOTPU");
+        service.urlParaBanco("http://200.129.71.228/horarios/data/professores.json");
+    }
+}
