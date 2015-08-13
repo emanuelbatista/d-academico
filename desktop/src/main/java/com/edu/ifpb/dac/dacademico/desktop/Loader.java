4a454a3c16c6f1a30a4e6cc278ea98be6a6d500a
Douglas Gabriel <douglasxgabriel@gmail.com>
2015-08-13 10:07:26 -0300

    fazendo chamada de método crawler da interface 'SalaService' na classe 'Loader'

--- desktop/src/main/java/com/edu/ifpb/dac/dacademico/desktop/Loader.java
+++ desktop/src/main/java/com/edu/ifpb/dac/dacademico/desktop/Loader.java
@@ -1,6 +1,8 @@
 package com.edu.ifpb.dac.dacademico.desktop;
 
 import com.edu.ifpb.dac.dacademico.core.servico.professor.ProfessorServiceImpl;
+import com.edu.ifpb.dac.dacademico.core.servico.sala.SalaService;
+import com.edu.ifpb.dac.dacademico.core.servico.sala.SalaServiceImpl;
 import java.io.IOException;
 
 /**
@@ -10,7 +12,10 @@ import java.io.IOException;
  */
 public class Loader {
     public static void main(String[] args) throws IOException {
-        ProfessorServiceImpl service = new ProfessorServiceImpl("com.edu.ifpb.dac.dacademico_entidades_jar_1.0-SNAPSHOTPU");
-        service.urlParaBanco("http://200.129.71.228/horarios/data/professores.json");
+        /*ProfessorServiceImpl service = new ProfessorServiceImpl("com.edu.ifpb.dac.dacademico_entidades_jar_1.0-SNAPSHOTPU");
+        service.urlParaBanco("http://200.129.71.228/horarios/data/professores.json");*/
+        
+        SalaService salaService = new SalaServiceImpl("com.edu.ifpb.dac.dacademico_entidades_jar_1.0-SNAPSHOTPU");
+        salaService.urlParaBanco("http://200.129.71.228/horarios/data/salas.json");
     }
 }
