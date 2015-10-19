93bac8692f71a1ab321a4c3a09a2965851aa3fb3
emanuelbatista <emanuelbatista2011@gmail.com>
2015-10-19 19:02:51 -0300

    alterando entidades

--- entidades/src/main/java/com/edu/ifpb/dac/dacademico/entidades/dominio/Pessoa.java
+++ entidades/src/main/java/com/edu/ifpb/dac/dacademico/entidades/dominio/Pessoa.java
@@ -5,6 +5,8 @@ import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 import javax.persistence.MappedSuperclass;
 
 /**
@@ -13,6 +15,7 @@ import javax.persistence.MappedSuperclass;
  * @version 0.1
  */
 @MappedSuperclass
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public abstract class Pessoa {
     
     @Id
