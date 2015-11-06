c6fba7340e40f176fab73ff99dbbef96c2841383
Douglas Gabriel <douglasxgabriel@gmail.com>
2015-11-06 11:42:26 -0300

    refatorando dependencias e estrutura do projeto

--- desktop/src/main/java/com/edu/ifpb/dac/dacademico/desktop/lookup/LookupPath.java
+++ desktop/src/main/java/com/edu/ifpb/dac/dacademico/desktop/lookup/LookupPath.java
@@ -7,15 +7,15 @@ package com.edu.ifpb.dac.dacademico.desktop.lookup;
  */
 public class LookupPath {
 
-    public static final String ADMIN_SERVICE = "java:global/D-Academico_-_Core/AdminServiceImpl";
-    public static final String ALUNO_SERVICE = "java:global/D-Academico_-_Core/AlunoServiceImpl";    
-    public static final String CURSO_SERVICE = "java:global/D-Academico_-_Core/CursoServiceImpl";    
-    public static final String DISCIPLINA_SERVICE = "java:global/D-Academico_-_Core/DisciplinaServiceImpl";    
-    public static final String PROFESSOR_SERVICE = "java:global/D-Academico_-_Core/ProfessorServiceImpl";    
-    public static final String TURMA_SERVICE = "java:global/D-Academico_-_Core/TurmaServiceImpl";    
-    public static final String AULA_SERVICE = "java:global/D-Academico_-_Core/AulaServicoImpl";    
-    public static final String HORARIO_SERVICE = "java:global/D-Academico_-_Core/HorarioServiceImpl";    
-    public static final String SALA_SERVICE = "java:global/D-Academico_-_Core/SalaServiceImpl";    
-    public static final String REPOSICAO_SERVICE = "java:global/D-Academico_-_Core/ReposicaoAulaServicoImpl";    
+    public static final String ADMIN_SERVICE = "java:global/web/AdminServiceImpl";
+    public static final String ALUNO_SERVICE = "java:global/web/AlunoServiceImpl";    
+    public static final String CURSO_SERVICE = "java:global/web/CursoServiceImpl";    
+    public static final String DISCIPLINA_SERVICE = "java:global/web/DisciplinaServiceImpl";    
+    public static final String PROFESSOR_SERVICE = "java:global/web/ProfessorServiceImpl";    
+    public static final String TURMA_SERVICE = "java:global/web/TurmaServiceImpl";    
+    public static final String AULA_SERVICE = "java:global/web/AulaServicoImpl";    
+    public static final String HORARIO_SERVICE = "java:global/web/HorarioServiceImpl";    
+    public static final String SALA_SERVICE = "java:global/web/SalaServiceImpl";    
+    public static final String REPOSICAO_SERVICE = "java:global/web/ReposicaoAulaServicoImpl";    
     
 }
