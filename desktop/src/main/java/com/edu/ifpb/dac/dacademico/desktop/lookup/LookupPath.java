cac8ca35d5bed59019206ebc1c7c84389bef1e38
Douglas Gabriel <douglasxgabriel@gmail.com>
2015-11-15 23:56:06 -0300

    resolvendo problema com mapeamento do serviço de turma

--- desktop/src/main/java/com/edu/ifpb/dac/dacademico/desktop/lookup/LookupPath.java
+++ desktop/src/main/java/com/edu/ifpb/dac/dacademico/desktop/lookup/LookupPath.java
@@ -12,7 +12,7 @@ public class LookupPath {
     public static final String CURSO_SERVICE = "java:global/D-Academico_-_Web/CursoServiceImpl!com.edu.ifpb.dac.dacademico.core.servico.curso.CursoServiceRemote";    
     public static final String DISCIPLINA_SERVICE = "java:global/D-Academico_-_Web/DisciplinaServiceImpl";    
     public static final String PROFESSOR_SERVICE = "java:global/D-Academico_-_Web/ProfessorServiceImpl";    
-    public static final String TURMA_SERVICE = "java:global/D-Academico_-_Web/TurmaServiceImpl";    
+    public static final String TURMA_SERVICE = "java:global/D-Academico_-_Web/TurmaServiceImpl!com.edu.ifpb.dac.dacademico.core.servico.turma.TurmaService";    
     public static final String AULA_SERVICE = "java:global/D-Academico_-_Web/AulaServicoImpl";    
     public static final String HORARIO_SERVICE = "java:global/D-Academico_-_Web/HorarioServiceImpl";    
     public static final String SALA_SERVICE = "java:global/D-Academico_-_Web/SalaServiceImpl";    
