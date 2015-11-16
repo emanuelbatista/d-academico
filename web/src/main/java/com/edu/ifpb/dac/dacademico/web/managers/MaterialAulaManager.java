1a435c7b2642244a737bbc7c5bf3186f1dc0d913
Douglas Gabriel <douglasxgabriel@gmail.com>
2015-11-15 22:23:58 -0300

    terminando de implementar upload e visualizacao de materiais de aula

--- web/src/main/java/com/edu/ifpb/dac/dacademico/web/managers/MaterialAulaManager.java
+++ web/src/main/java/com/edu/ifpb/dac/dacademico/web/managers/MaterialAulaManager.java
@@ -2,14 +2,14 @@ package com.edu.ifpb.dac.dacademico.web.managers;
 
 import com.edu.ifpb.dac.dacademico.core.exceptions.EntidadeInexistenteException;
 import com.edu.ifpb.dac.dacademico.core.servico.materialaula.MaterialAulaService;
+import com.edu.ifpb.dac.dacademico.core.servico.professor.ProfessorService;
 import com.edu.ifpb.dac.dacademico.core.servico.turma.TurmaService;
 import com.edu.ifpb.dac.dacademico.entidades.dominio.MaterialAula;
+import com.edu.ifpb.dac.dacademico.entidades.dominio.Professor;
 import com.edu.ifpb.dac.dacademico.entidades.dominio.Turma;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.ejb.EJB;
 import javax.enterprise.context.SessionScoped;
 import javax.faces.context.FacesContext;
@@ -33,16 +33,21 @@ public class MaterialAulaManager implements Serializable{
     private MaterialAulaService materialService;
     @EJB
     private TurmaService turmaService;
+    @EJB
+    private ProfessorService professorService;
     private HttpSession session = (HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(true);
     private Long[] turmas;
     
     public String upload (){
         try{
-            for (Turma turma : getTurmasSelecionadas(turmas)){
+            List<Turma> listaTurmas = getTurmasSelecionadas(turmas);
+            for (Turma turma : listaTurmas){
                 MaterialAula material = new MaterialAula();
                 material.setNome(file.getSubmittedFileName());
                 materialService.salvar(turma, material, IOUtils.toByteArray(file.getInputStream()));               
             }
+            Professor prof = ((Professor) session.getAttribute("professor"));
+            session.setAttribute("professor", professorService.buscar(prof.getCod()));
         }catch (Exception e){
             e.printStackTrace();
         }
